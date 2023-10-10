ebdd9eaea287c0cb74bedc991d7b4bd0c3d00e3c
RavenTheNecromancer <dimitar.boyadzhiev02@gmail.com>
2023-10-10 19:31:03 +0300

    Added Animations
    Added and set up animations working on the character. Transitions are set up and working via boolean also accessed in the script.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
 
+	[SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
 	private void Update()
@@ -16,15 +17,19 @@ public class Unit : MonoBehaviour
 			Vector3 moveDirection = (targetPosition - transform.position).normalized;
 			float moveSpeed = 4f;
 			transform.position += moveDirection * moveSpeed * Time.deltaTime;
-		}
-		
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			Move(MouseWorld.GetPosition());
+			float rotateSpeed = 8f;
+			transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+			unitAnimator.SetBool("IsWalking", true);
 		}
+		else
+		{
+			unitAnimator.SetBool("IsWalking", false);
+		}
+
 	}
-	private void Move(Vector3 targetPosition)
+	public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    [SerializeField] private Unit selectedUnit;
+
+	private void Update()
+	{
+
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			selectedUnit.Move(MouseWorld.GetPosition());
+		}
+	}
+}
